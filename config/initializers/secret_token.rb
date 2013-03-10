ebf5c838f7e7e626f7d81961d016444e081b6d07
Kinan Muhammed <kinanmuhammed@hotmail.com>
2013-03-10 13:24:01 +0000

    ruby

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PatientRecord::Application.config.secret_token = '8fd7acbee410ae621b27cc0c9e6a96ab70f16bd5a831951b8a313d326d23a21af0110a7c5621e67e11b71dbe0ffe403c1730219faed0fbb6fbecc41694556e5c'
